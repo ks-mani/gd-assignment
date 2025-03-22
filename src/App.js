d560523d94179f7fb4ecfc1361c1b1e0ce549a59
Mani Kumar Srivastava <kumar.mani017@gmail.com>
2025-03-23 02:14:57 +0530

    Data Fetch

--- src/App.js
+++ src/App.js
@@ -1,37 +1,62 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import WidgetContainer from "./WidgetContainer";
 
-const ListCard = () => {
+const ListCard = ({
+  cardData: {
+    name = "",
+    description = "",
+    language = "",
+    updatedAt = "",
+    url = "",
+    visibility = "",
+  },
+}) => {
   return (
     <div className={styles.listCardWrapper}>
       <div className={styles.title}>
-        <h2>sunt in culpa qui officia deserunt mollit anim.</h2>
-        <span className={styles.tag}>public</span>
+        <h2>{name}</h2>
+        <span className={styles.tag}>{visibility}</span>
       </div>
 
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat
-      </p>
+      <p>{description}</p>
       <div className={styles.meta}>
-        <span className={styles.lang}>HTML</span>
+        <span className={styles.lang}>{language}</span>
         <span className={styles.dot}></span>
-        <span className={styles.updated}>Updated on 16 July, 2023</span>
+        <span className={styles.updated}>Updated on {updatedAt}</span>
       </div>
     </div>
   );
 };
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/godaddy/repos")
+      .then((res) => res.json())
+      .then((data) => {
+        const formattedData = data.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            language: item.language,
+            updatedAt: item.updated_at,
+            url: item.url,
+            visibility: item.visibility,
+          };
+        });
+
+        console.log(formattedData);
+        setData(formattedData);
+      });
+  }, []);
   return (
     <WidgetContainer>
       <h1>List of GoDaddy's Github Repo</h1>
-      {new Array(10).fill(0).map((item) => (
-        <ListCard />
-      ))}
+      {data && data.map((item) => <ListCard cardData={item} />)}
     </WidgetContainer>
   );
 }
