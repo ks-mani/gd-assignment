80daf92dce672bf32ce8a3aa8e8c9eee226cc53e
Mani Kumar Srivastava <kumar.mani017@gmail.com>
2025-03-23 15:20:05 +0530

    Procuring the data in the detail view

--- src/RepoDetail/index.js
+++ src/RepoDetail/index.js
@@ -1,12 +1,18 @@
 import { useParams } from "react-router";
 import WidgetContainer from "../WidgetContainer";
+import { useFullRepoDataStore } from "../store/repoData";
 
 const RepoDetail = () => {
-  let { repoId } = useParams();
+  const { repoId } = useParams();
+  const { fullRepoData } = useFullRepoDataStore();
 
   return (
     <WidgetContainer>
       <h1>Hello there.. This is the detail view of {repoId}</h1>
+
+      {JSON.stringify(
+        fullRepoData.filter((item) => item.id.toString() === repoId)
+      )}
     </WidgetContainer>
   );
 };
